a6a9bbcf369221155bb15e2457565a3e04ec6837
talshachar20 <talshachar@sponsorpay.com>
2015-03-04 11:35:34 +0100

    add session validation

--- app/controllers/memo_cards_controller.rb
+++ app/controllers/memo_cards_controller.rb
@@ -96,8 +96,9 @@ class MemoCardsController < ApplicationController
   end
 
   def count_for_result()
-    result_correct = Results.select(:id).where(is_correct: 1 , user_id: current_user).length.to_s
-    result_wrong = Results.select(:id).where(is_correct: 0 , user_id: current_user).length.to_s
+    session_id = session[:session_id]
+    result_correct = Results.select(:id).where(is_correct: 1 , user_id: current_user , session_id: session_id ).length.to_s
+    result_wrong = Results.select(:id).where(is_correct: 0 , user_id: current_user , session_id: session_id).length.to_s
     respond_to do |format|
       format.json { render json: {right_answers: result_correct, wrong_answers: result_wrong} }
     end
