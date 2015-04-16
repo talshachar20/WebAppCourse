c2530619bc43ff7aa4fed71aecef594fc7471dca
talshachar20 <talshachar@sponsorpay.com>
2015-04-16 18:41:49 +0200

    added admin user and delete and edit memo cards only for him

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415154748) do
+ActiveRecord::Schema.define(version: 20150416163258) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150415154748) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_type"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
