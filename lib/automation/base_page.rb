da96e0374be3fd03e8999dfee3bcebd4e0f55ec9
HerrShabi <raz.shuty@gmail.com>
2015-06-13 12:45:50 +0200

    Update base_page.rb

--- lib/automation/base_page.rb
+++ lib/automation/base_page.rb
@@ -4,6 +4,14 @@ class Base
     ENV['base_url'] = 'http://0.0.0.0:3000/'
 
     def initialize
+      # This is being called for each page object you will create!!!
+      # Hence for each page object initialized you will recreate the @driver and open a new browser
+      # Therefor her lies your problem!
+      # There are many ways to handle this, one is not to create the driver in the Base initialize...
+      # Or if you feel you want to do that, than you will need to change this method to be like so:
+      #
+      # if @driver then @driver else @driver = Selenium::WebDriver.for :chrome
+      #
       @driver = Selenium::WebDriver.for :chrome #, :profile => profile
     end
 
