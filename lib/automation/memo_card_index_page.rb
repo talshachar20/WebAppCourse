8a5d9c9bf86473289aef5298acd19f845c6bd4bf
talshachar20 <talshachar@sponsorpay.com>
2015-07-14 16:48:00 +0200

    selector_Builder

--- lib/automation/memo_card_index_page.rb
+++ lib/automation/memo_card_index_page.rb
@@ -12,16 +12,20 @@ class MemoCardIndexPage < Base
     super(driver)
     initialize_selectors
   end
-  @@index = ""
 
   def initialize_selectors
     @selectors = {
         :NEW_DESIGN_TEXT => {id: 'newDesigdn'} ,
         :WORD_STATUS_COLUMN =>  {id: 'statusBar'} ,
         :MEMO_CARD_LIST => {xpath:  '/html/body/table/tbody'} ,
-        :MEMO_CARD_BY_INDEX =>   {xpath:   'id("newDesign")/x:tbody/x:tr/x:td[6]'}
-        #:gerne_selected_tag => lambda { |tag| Locator.new(:list , :xpath, "//div[@id='program_region_landing_pages_attributes_0_app_genre_category_ids_chzn']/ul[@class='chzn-choices']//span[text()='#{tag}']/following-sibling::a") }
+        #:MEMO_CARD_BY_INDEX=> {:xpath=> "id('newDesign')/x:tbody/x:tr/x:td[6]"}
     }
+  end
+
+  def selector_builder(selector_string, param)
+     selector = "id('newDesign')/x:tbody/x:tr/x:td[6]"
+     selector_key = ':MEMO_CARD_BY_INDEX'
+     return  selector_key =>   {xpath: selector}
   end
 
   def new_design_status
@@ -35,7 +39,8 @@ class MemoCardIndexPage < Base
   end
 
   def choose_memo_card_by_index(index) #TODO - maybe build an array table
-    value = find ("id('newDesign')/x:tbody/x:tr/x:td[5]/x:a")
-    puts 'value is: ' + value
+    binding.pry
+    puts selector_builder("cc", "1")
+    #puts 'value is: ' + value
   end
 end
