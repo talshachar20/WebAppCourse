04e0db0391c7c98b5c31364f8706c7fb096bcfe9
talshachar20 <talshachar@sponsorpay.com>
2015-10-19 17:55:25 +0200

    add modules and updated ignore

--- app/controllers/memo_cards_controller.rb
+++ app/controllers/memo_cards_controller.rb
@@ -1,6 +1,8 @@
 require 'pstore'
 require_relative '../../lib/answer_selector'
+require_relative '../../lib/check_answers'
 include AnswerSelector
+include CheckAnswers
 
 class MemoCardsController < ApplicationController
   before_action :set_memo_card, only: [:show, :edit, :update, :destroy]
@@ -81,23 +83,8 @@ class MemoCardsController < ApplicationController
   end
 
 
-  def check_answer()
-    word_in_page = params[:word_temp]
-    word_in_german = params[:word_in_german]
-    right_answer = MemoCard.select("id").where(translation: word_in_page , word: word_in_german)
-    answer_id = MemoCard.select("id").where(word: word_in_german).first.to_param
-    next_answer = get_next_word_id(answer_id)
-    if right_answer.empty?
-      wrong_answer_to_result(answer_id)
-      respond_to do |format|
-        format.json { render json: {answer:"false" , nextid:next_answer} }
-      end
-    else
-      correct_answer_to_result(answer_id)
-      respond_to do |format|
-        format.json { render json: {answer:"true" , nextid:next_answer } }
-      end
-    end
+  def check_answer
+    check_answer_from_module
   end
 
   def count_for_result()
@@ -129,30 +116,6 @@ class MemoCardsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def memo_card_params
       params.require(:memo_card).permit(:word , :translation, :word_id , :lang_id)
-    end
-
-    def get_next_word_id(answer_id)
-      next_word_id = MemoCard.select("id").where("id >" + answer_id).where(lang_id: current_user.user_type).first.to_param
-      if next_word_id == nil
-         next_word_id = MemoCard.select("id").where("id <=" + answer_id).where(lang_id: current_user.user_type).first.to_param
-         return next_word_id
-      else
-        return next_word_id
-      end
-    end
-
-    def correct_answer_to_result(answer_id)
-      the_current_user = current_user.id
-      session_id = session[:session_id]
-      Results.create(:user_id => the_current_user , :word_id => answer_id , :is_correct => 1 , :session_id => session_id)
-      logger.debug "True result entered to user id:  #{the_current_user}"
-    end
-
-    def wrong_answer_to_result(answer_id)
-      the_current_user = current_user.id
-      session_id = session[:session_id]
-      Results.create(:user_id => the_current_user , :word_id => answer_id , :is_correct => 0 , :session_id => session_id)
-      logger.debug "Wrong result entered to user id:  #{the_current_user}"
     end
 
 end
---
+++ lib/check_answers.rb
@@ -0,0 +1,44 @@
+module CheckAnswers
+  def check_answer_from_module
+    word_in_page = params[:word_temp]
+    word_in_german = params[:word_in_german]
+    right_answer = MemoCard.select("id").where(translation: word_in_page , word: word_in_german)
+    answer_id = MemoCard.select("id").where(word: word_in_german).first.to_param
+    next_answer = get_next_word_id(answer_id)
+    if right_answer.empty?
+      wrong_answer_to_result(answer_id)
+      respond_to do |format|
+        format.json { render json: {answer:"false" , nextid:next_answer} }
+      end
+    else
+      correct_answer_to_result(answer_id)
+      respond_to do |format|
+        format.json { render json: {answer:"true" , nextid:next_answer } }
+      end
+    end
+  end
+
+  def get_next_word_id(answer_id)
+    next_word_id = MemoCard.select("id").where("id >" + answer_id).where(lang_id: current_user.user_type).first.to_param
+    if next_word_id == nil
+      next_word_id = MemoCard.select("id").where("id <=" + answer_id).where(lang_id: current_user.user_type).first.to_param
+      return next_word_id
+    else
+      return next_word_id
+    end
+  end
+
+  def correct_answer_to_result(answer_id)
+    the_current_user = current_user.id
+    session_id = session[:session_id]
+    Results.create(:user_id => the_current_user , :word_id => answer_id , :is_correct => 1 , :session_id => session_id)
+    logger.debug "True result entered to user id:  #{the_current_user}"
+  end
+
+  def wrong_answer_to_result(answer_id)
+    the_current_user = current_user.id
+    session_id = session[:session_id]
+    Results.create(:user_id => the_current_user , :word_id => answer_id , :is_correct => 0 , :session_id => session_id)
+    logger.debug "Wrong result entered to user id:  #{the_current_user}"
+  end
+end
